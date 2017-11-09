6e54106121a576e0ec17c63e7627def65d786892
sunkanggao <sunkanggao1992@gmail.com>
2017-11-09 20:45:41 +0800

    利用链接文本

--- 4_Searching_and_Ranking/searchengine.py
+++ 4_Searching_and_Ranking/searchengine.py
@@ -82,7 +82,16 @@ class crawler:
 
     # 添加一个关联两个网页的链接
     def addlinkref(self, urlFrom, urlTo, linkText):
-        pass
+        words = self.separatewords(linkText)
+        fromid = self.getentryid('urllist', 'url', urlFrom)
+        toid = self.getentryid('urllist', 'url', urlTo)
+        if fromid == toid: return
+        cur = self.con.execute('insert into link(fromid, toid) values(%d, %d)' % (fromid, toid))
+        linkid = cur.lastrowid
+        for word in words:
+            if word in ignorewords: continue
+            wordid = self.getentryid('wordlist', 'word', word)
+            self.con.execute('insert into linkwords(linkid, wordid) values(%d, %d)' % (linkid, wordid))
 
     # 从一小组网页开始进行广度优先搜索，直至某一给定深度，期间为网页建立索引
     def crawl(self, pages, depth=2):
@@ -129,6 +138,37 @@ class crawler:
         self.con.execute('create index urlfromidx on link(fromid)')
         self.dbcommit()
 
+    # PageRank算法
+    def calculatepagerank(self, iterations=20):
+        # 清楚当前pagerank表
+        self.con.execute('drop table if exists pagerank')
+        self.con.execute('create table pagerank(urlid primary key, score)')
+
+        # 初始化每个url，令其pagerank值都为1
+        self.con.execute('insert into pagerank select rowid, 1.0 from urllist')
+        self.dbcommit()
+
+        for i in range(iterations):
+            print 'Iteration %d' % i
+            for (urlid, ) in self.con.execute('select rowid from urllist'):
+                pr = 0.15
+                # 循环遍历指向当前网页的所有其他网页
+                for (linker, ) in self.con.execute(
+                        'select distinct fromid from link where toid=%d' % urlid):
+                    # 得到链接源对应网页的pagerank值
+                    linkingpr = self.con.execute(
+                        'select score from pagerank where urlid=%d' % linker
+                    ).fetchone()[0]
+                    # 根据链接源求得总的链接数
+                    linkingcount = self.con.execute(
+                        'select count(*) from link where fromid=%d' % linker
+                    ).fetchone()[0]
+                    pr += 0.85 * (linkingpr/linkingcount)
+                self.con.execute(
+                    'update pagerank set score=%f where urlid=%d' % (pr, urlid)
+                )
+            self.dbcommit()
+
 
 class searcher:
     def __init__(self, dbname):
@@ -137,6 +177,7 @@ class searcher:
     def __del__(self):
         self.con.close()
 
+    # row的格式（urlid, w1location, w2location, ..., wnlocation）
     def getmatchrows(self, q):
         # 构造查询的字符串
         fieldlist = 'w0.urlid'
@@ -175,8 +216,9 @@ class searcher:
         totalscores = dict([(row[0], 0) for row in rows])
 
         weights = [(1.0, self.frequencyscore(rows)),
-                   (1.5, self.locationscore(rows)),
-                   (1.5, self.distancescore(rows))]
+                   (1.0, self.locationscore(rows)),
+                   (1.0, self.pagerankscore(rows)),
+                   (1.0, self.linktextscore(rows, wordids))]
 
         for (weight, scores) in weights:
             for url in totalscores:
@@ -238,6 +280,32 @@ class searcher:
                              for u in uniqueurls])
         return self.normalizescores(inboundcount)
 
+    # pagerank评分
+    def pagerankscore(self, rows):
+        pageranks = dict([(row[0], self.con.execute(
+            'select score from pagerank where urlid=%d' % row[0]
+        ).fetchone()[0]) for row in rows])
+        maxrank = max(pageranks.values())
+        normalizedscores = dict([(u, float(l) / maxrank) for (u, l) in pageranks.items()])
+        return normalizedscores
+
+    # 利用链接文本
+    def linktextscore(self, rows, wordids):
+        linkscores = dict([(row[0], 0) for row in rows])
+        for wordid in wordids:
+            cur = self.con.execute(
+                'select link.fromid, link.toid from linkwords, link where wordid=%d and linkwords.linkid=link.rowid'
+            % wordid)
+            for (fromid, toid) in cur:
+                if toid in linkscores:
+                    pr = self.con.execute(
+                        'select score from pagerank where urlid=%d' % fromid
+                    ).fetchone()[0]
+                    linkscores[toid] += pr
+        maxscore = max(linkscores.values())
+        normalizedscores = dict([(u, float(l) / maxscore) for (u, l) in linkscores.items()])
+        return normalizedscores
+
 
 if __name__ == '__main__':
     # 爬取网页，建立数据库
@@ -245,6 +313,7 @@ if __name__ == '__main__':
     # crawler = crawler('Functional_programming.db')
     # crawler.createindextables()
     # crawler.crawl(pageList)
+    # crawler.calculatepagerank()
 
     # 查询
     e = searcher('Functional_programming.db')
